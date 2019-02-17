856d51f0184fd43c50cb507035ed74377990da3a
Unknown <leonis13579@gmail.com>
2019-02-17 08:54:15 +0300

    UnityClient made, not tested

---
+++ RoomAliveUnity/Assets/Scripts/Connection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Networking;
+using Random = UnityEngine.Random;
+
+
+public class Connection : MonoBehaviour
+{
+    private string ConnPass = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            ConnPass += Random.Range(0, 9);
+        }
+
+        Thread thread = new Thread(new ThreadStart(AnswerWaiting));
+    }
+
+    private void AnswerWaiting()
+    {
+        IPEndPoint ippoint = new IPEndPoint(IPAddress.Any, 65000);
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            socket.Bind(ippoint);
+            socket.Listen(1);
+
+            print("tcpServer запущен");
+
+            bool answer = false;
+            while (!answer)
+            {
+                Socket handler = socket.Accept();
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+                byte[] data = new byte[256];
+
+                bytes = handler.Receive(data);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+
+                if (builder.ToString() == ConnPass)
+                {
+                    data = Encoding.Unicode.GetBytes("Подключен");
+                    handler.Send(data);
+
+                    GameObject parent = transform.parent.gameObject;
+                    parent.transform.Find("MyRoom").gameObject.SetActive(true);
+
+                    MyLanBroadcast lanBroadcast = gameObject.GetComponent<MyLanBroadcast>();
+                    lanBroadcast.alive = false;
+                }
+                else
+                {
+                    data = Encoding.Unicode.GetBytes("Неверный пароль");
+                    handler.Send(data);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            print(ippoint.Address.ToString());
+            print("Kinect не запущен");
+            print(ex.Message);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RoomAliveUnity/Assets/Scripts/MyLanBroadcast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyLanBroadcast : NetworkDiscovery
+{
+    public bool alive = true;
+
+    public void StartBroadcast()
+    {
+        StopBroadcast();
+        base.Initialize();
+        base.StartAsServer();
+    }
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        base.OnReceivedBroadcast(fromAddress, data);
+    }
+
+    private void Update()
+    {
+        if (!alive)
+        {
+            StopBroadcast();
+        }
+    }
+}
